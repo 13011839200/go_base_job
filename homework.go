5da7b1a6c0a3bf54b0f3be33fdef2f0f0d2a8cb6
13011839200 <m13011839200@163.com>
2025-12-29 17:16:49 +0800

    完成

--- homework.go
+++ homework.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"sort"
 	"strconv"
 )
 
@@ -46,43 +47,144 @@ func IsPalindrome(num int) bool {
 // 3. 有效的括号
 // 给定一个只包括 '(', ')', '{', '}', '[', ']' 的字符串，判断字符串是否有效
 func IsValid(s string) bool {
-	// TODO: implement
-	return false
+
+	n := len(s)
+	if n%2 == 1 {
+		return false
+	}
+	pairs := map[byte]byte{
+		')': '(',
+		']': '[',
+		'}': '{',
+	}
+	stack := []byte{}
+	for i := 0; i < n; i++ {
+		if pairs[s[i]] > 0 {
+			if len(stack) == 0 || stack[len(stack)-1] != pairs[s[i]] {
+				return false
+			}
+			stack = stack[:len(stack)-1]
+		} else {
+			stack = append(stack, s[i])
+		}
+	}
+	return len(stack) == 0
+}
+
+func lcp(str1, str2 string) string {
+	length := min(len(str1), len(str2))
+	index := 0
+	for index < length && str1[index] == str2[index] {
+		index++
+	}
+	return str1[:index]
+}
+
+func min(x, y int) int {
+	if x < y {
+		return x
+	}
+	return y
 }
 
 // 4. 最长公共前缀
 // 查找字符串数组中的最长公共前缀
 func LongestCommonPrefix(strs []string) string {
-	// TODO: implement
-	return ""
+	if len(strs) == 0 {
+		return ""
+	}
+	prefix := strs[0]
+	count := len(strs)
+	for i := 1; i < count; i++ {
+		prefix = lcp(prefix, strs[i])
+		if len(prefix) == 0 {
+			break
+		}
+	}
+	return prefix
+
 }
 
 // 5. 加一
 // 给定一个由整数组成的非空数组所表示的非负整数，在该数的基础上加一
 func PlusOne(digits []int) []int {
-	// TODO: implement
-	return nil
+	n := len(digits)
+	for i := n - 1; i >= 0; i-- {
+		if digits[i] != 9 {
+			digits[i]++
+			for j := i + 1; j < n; j++ {
+				digits[j] = 0
+			}
+			return digits
+		}
+	}
+	// digits 中所有的元素均为 9
+
+	digits = make([]int, n+1)
+	digits[0] = 1
+	return digits
+
 }
 
 // 6. 删除有序数组中的重复项
 // 给你一个有序数组 nums ，请你原地删除重复出现的元素，使每个元素只出现一次，返回删除后数组的新长度。
 // 不要使用额外的数组空间，你必须在原地修改输入数组并在使用 O(1) 额外空间的条件下完成。
 func RemoveDuplicates(nums []int) int {
-	// TODO: implement
-	return 0
+	n := len(nums)
+	if n == 0 {
+		return 0
+	}
+	slow := 1
+	for fast := 1; fast < n; fast++ {
+		if nums[fast] != nums[fast-1] {
+			nums[slow] = nums[fast]
+			slow++
+		}
+	}
+	return slow
+
 }
 
 // 7. 合并区间
 // 以数组 intervals 表示若干个区间的集合，其中单个区间为 intervals[i] = [starti, endi] 。
 // 请你合并所有重叠的区间，并返回一个不重叠的区间数组，该数组需恰好覆盖输入中的所有区间。
 func Merge(intervals [][]int) [][]int {
-	// TODO: implement
-	return nil
+	ans := [][]int{}
+	if len(intervals) == 0 {
+		return ans
+	}
+	sort.Slice(intervals, func(i, j int) bool {
+		return intervals[i][0] < intervals[j][0]
+	})
+	ans = append(ans, intervals[0])
+	for i := 1; i < len(intervals); i++ {
+		last := ans[len(ans)-1]
+		if last[1] < intervals[i][0] {
+			ans = append(ans, intervals[i])
+		} else {
+			last[1] = max(last[1], intervals[i][1])
+		}
+	}
+	return ans
+
+}
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
 }
 
 // 8. 两数之和
 // 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那两个整数
 func TwoSum(nums []int, target int) []int {
-	// TODO: implement
+	for i, x := range nums {
+		for j := i + 1; j < len(nums); j++ {
+			if x+nums[j] == target {
+				return []int{i, j}
+			}
+		}
+	}
 	return nil
+
 }
